b83c385187b0d84047ef9aeb41d238aea3c82c28
liuzengwei <604806892@qq.com>
2021-10-28 17:26:13 +0800

    version

---
+++ docs/.vuepress/components/version.vue
@@ -0,0 +1,15 @@
+<template>
+    <span>
+        当前版本：{{version}}
+    </span>
+</template>
+<script>
+import pak from '../../../package.json'
+export default {
+    data() {
+        return {
+            version:pak.version||'---'
+        }
+    },
+}
+</script>
